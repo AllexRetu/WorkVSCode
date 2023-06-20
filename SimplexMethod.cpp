f62654b111752f82106aa3c551e420d5bff28d6a
AlexRetu <alex.trembach2015@yandex.ru>
2023-06-20 14:34:40 +0500

    Проверка

--- SimplexMethod.cpp
+++ SimplexMethod.cpp
@@ -1,10 +1,67 @@
+#include <iostream>
 #include <cmath>
 #include <vector>
+#include <string>
 
-template<typename Matrix>
-Matrix composition(Matrix matrix1, Matrix matrix2)
+using namespace std;
+
+using Matrix = vector<vector<double>>;
+
+void printMatrix(Matrix const & matrix, string name)
 {
-    size_t col = matrix2.at(0).size();
-    size_t row = matrix.size();
-    Matrix matrix = Matrix(row, typeOf())
+	cout << name << ": " << endl;
+	for (int i = 0; i < matrix.size(); ++i)
+	{
+		for (int j = 0; j < matrix.at(i).size(); ++j)
+			cout << matrix[i][j] << " ";
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+void printMatrix(Matrix const & matrix)
+{
+	for (int i = 0; i < matrix.size(); ++i)
+	{
+		for (int j = 0; j < matrix.at(i).size(); ++j)
+			cout << matrix[i][j] << " ";
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+Matrix product(Matrix const & matrix1, Matrix const & matrix2)
+{
+    size_t cols = matrix2.at(0).size();
+    size_t rows = matrix1.size();
+    size_t p = matrix1.at(0).size();
+    if (p != matrix2.size()) // размеры не совместны
+        return Matrix();
+    Matrix product = Matrix(rows, vector<double>(cols));
+
+    for (int i = 0; i < rows; ++i)
+        for (int j = 0; j < cols; ++j)
+            for (int k = 0; k < p; ++k)
+                product[i][j] += matrix1[i][k]*matrix2[k][j];
+    return product;
+}
+
+
+int main()
+{
+    Matrix A
+    {
+        {1,2,3},
+        {3,2,1},
+        {2,2,0}
+    },
+    B
+    {
+        {1,0},
+        {0,3},
+        {1,2}
+    };
+
+    Matrix C = product(A,B);
+    printMatrix(C);
 }
