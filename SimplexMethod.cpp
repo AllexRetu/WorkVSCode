aade09d01853c90e38bbc3e237d526aaf5825b9a
AlexRetu <alex.trembach2015@yandex.ru>
2023-06-20 13:53:02 +0500

    просто сейв

---
+++ SimplexMethod.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+#include <vector>
+
+template<typename Matrix>
+Matrix composition(Matrix matrix1, Matrix matrix2)
+{
+    size_t col = matrix2.at(0).size();
+    size_t row = matrix.size();
+    Matrix matrix = Matrix(row, typeOf())
+}
