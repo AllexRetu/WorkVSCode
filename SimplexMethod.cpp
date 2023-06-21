35e38ccce8703b607bc3085a96ddd3533032445f
AlexRetu <alex.trembach2015@yandex.ru>
2023-06-21 17:08:35 +0500

    Вывод того что есть

--- SimplexMethod.cpp
+++ SimplexMethod.cpp
@@ -12,7 +12,7 @@ pair<double, Matrix> simplexMethod(Matrix const & A, Matrix const & b, Matrix co
     size_t m = A.size();                                                                            // c - строка
     size_t n = A.at(0).size();
 
-    if (m != b.at(0).size() || n != c.size() || b.size() != 1 || c.at(0).size() != 1) // проверка на корректность данных
+    if (c.size() != 1 || b.at(0).size() != 1 || m != b.size() || n != c.at(0).size()) // проверка на корректность данных
         return pair<double, Matrix>();
 
     Matrix E = createMatrix(m,m);   // создаем единичную матрицу
@@ -27,6 +27,9 @@ pair<double, Matrix> simplexMethod(Matrix const & A, Matrix const & b, Matrix co
         c1[0][i] = -1e+5;   // -10^5
 
     Matrix c_ = (c1|c);
+
+    cout << A_ << endl;
+    cout << c_ << endl;
 
     Matrix X = createMatrix(n+m,1);
 
@@ -47,32 +50,36 @@ int main()
 {
     Matrix A
     {
-        {1,2,3,},
-        {3,2,1,},
-        {2,2,0,},
+        {1,2,3,3},
+        {3,2,1,4},
+        {2,2,0,6},
         /*{1,-4,-7,1,4,0},
         {6,0,1,1,1,1},
         {0,9,11,2,0,1}*/
     },
-    B
+    b
     {
-        {1,0},
-        {0,3},
-        {1,2}
+        {1,},
+        {0,},
+        {1,}
+    },
+    c
+    {
+        {5,6,-1,6},
     };
 
-    cout << det(A) << endl;
+    /*cout << det(A) << endl;
     cout << perRow(A,0,1) << endl;
 
     cout << inverse(A) << endl;
     cout << proof(A) << endl;
 
-    cout << 5*(A*B)+B << endl;
+    cout << 5*(A*b)+b << endl;
 
-    Matrix C = (A|B);
+    Matrix C = (A|b);
 
     cout << disjoin(C,3).first << endl;
-    cout << disjoin(C,3).second << endl;
+    cout << disjoin(C,3).second << endl;*/
     
-
+    simplexMethod(A,b,c);
 }
